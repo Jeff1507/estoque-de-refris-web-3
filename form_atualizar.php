ec720ec670c2a5303a64cc95516277e371802065
Jefferson <Dibloob@outlook.com>
2024-10-11 13:04:30 -0300

    formularios estilizados

--- form_atualizar.php
+++ form_atualizar.php
@@ -20,25 +20,75 @@
     <title>Atualizar Refrigerante</title>
 </head>
 <body>
-    <section>
-        <div>
-            
+    <section class="flex items-center justify-center min-h-screen">
+        <div class="w-[400px] flex flex-col gap-8">
+            <img src="refri.png" alt="" class="w-[200px] self-center">
+            <h3 class="text-center text-2xl font-semibold">
+                Atualizar Refrigerante
+            </h3>
+            <form method="POST" action="atualizaRefri.php" class="flex flex-col gap-5">
+                <input type="hidden" name="id" value="<?php echo $refriSelecionado['id']; ?>">
+                <div class="relative">
+                    <input 
+                        type="text" 
+                        name="marca" 
+                        id="marca"
+                        required 
+                        value="<?php echo $refriSelecionado['marca']; ?>"
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" 
+
+                    />
+                    <label 
+                        for="marca"
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                    >
+                        Marca
+                    </label>
+                </div>
+                <div class="relative">
+                    <input 
+                        type="text" 
+                        name="sabor" 
+                        id="sabor"
+                        required 
+                        value="<?php echo $refriSelecionado['sabor']; ?>"
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
+                    />
+                    <label 
+                        for="sabor"
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+
+                    >
+                        Sabor
+                    </label>
+                </div>
+                <div class="relative">
+                    <input 
+                        type="text" 
+                        name="quantidade" 
+                        id="quantidade"
+                        required 
+                        value="<?php echo $refriSelecionado['quantidade']; ?>"
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
+                    />
+                    <label 
+                        for="quantidade"
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                    >
+                        Quantidade (em Litros)
+                    </label>
+                </div>
+
+                <button 
+                    type="submit"
+                    class="group w-full self-end inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-purple-500 px-6 font-medium text-white transition hover:bg-purple-500/70 hover:text-white/70"
+                >
+                    Atualizar
+                </button>
+            </form>
         </div>
+
     </section>
-    <h3>Atualizar Refrigerante</h3>
-    <form method="POST" action="atualizaRefri.php">
-        <input type="hidden" name="id" value="<?php echo $refriSelecionado['id']; ?>">
-
-        <label for="marca">Marca</label>
-        <input type="text" name="marca" required value="<?php echo $refriSelecionado['marca']; ?>">
-
-        <label for="sabor">Sabor</label>
-        <input type="text" name="sabor" required value="<?php echo $refriSelecionado['sabor']; ?>">
-
-        <label for="quantidade">Quantidade (em Litros)</label>
-        <input type="text" name="quantidade" required value="<?php echo $refriSelecionado['quantidade']; ?>">
-
-        <button type="submit">Atualizar</button>
-    </form>
+    
 </body>
 </html>
--- form_cadastro.php
+++ form_cadastro.php
@@ -20,12 +20,12 @@
                         name="marca" 
                         id="marca"
                         required
-                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" 
                         placeholder=" "
                     />
                     <label 
                         for="marca" 
-                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"            >
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"            >
                         Marca
                     </label>
                 </div>
@@ -35,12 +35,12 @@
                         name="sabor" 
                         id="sabor"
                         required
-                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" 
                         placeholder=" "
                     />
                     <label 
                         for="sabor"
-                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
                     >
                         Sabor
                     </label>
@@ -51,12 +51,12 @@
                         name="quantidade" 
                         id="quantidade"
                         required
-                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                        class="block rounded-md px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" 
                         placeholder=" "
                     />
                     <label 
                         for="quantidade"
-                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                        class="cursor-text absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
                     >
                         Quantidade (em Litros)
                     </label>
