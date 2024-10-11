4739a3875c3e0f3183acd0f57b11b139603d7197
Jefferson <Dibloob@outlook.com>
2024-10-11 12:10:09 -0300

    👌

--- form_atualizar.php
+++ form_atualizar.php
@@ -16,9 +16,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="output.css" rel="stylesheet">
+    <title>Atualizar Refrigerante</title>
 </head>
 <body>
+    <section>
+        <div>
+            
+        </div>
+    </section>
     <h3>Atualizar Refrigerante</h3>
     <form method="POST" action="atualizaRefri.php">
         <input type="hidden" name="id" value="<?php echo $refriSelecionado['id']; ?>">
--- form_cadastro.php
+++ form_cadastro.php
@@ -8,11 +8,12 @@
 </head>
 <body>
     <section class="flex items-center justify-center min-h-screen">
-        <div class="w-[400px] border-2 border-gray-300 rounded-2xl flex flex-col gap-8 px-5 py-8">
+        <div class="w-[400px] flex flex-col gap-8">
+            <img src="refri.png" alt="" class="w-[200px] self-center">
             <h3 class="text-center text-2xl font-semibold">
                 Adicionar Refrigerante
             </h3>
-            <form method="POST" action="cadastroRefri.php" class="grid space-y-5">
+            <form method="POST" action="cadastroRefri.php" class="flex flex-col gap-5">
                 <div class="relative">
                     <input 
                         type="text" 
@@ -60,20 +61,11 @@
                         Quantidade (em Litros)
                     </label>
                 </div>
-                <br/>
                 <button 
                     type="submit"
-                    class="group w-full relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-purple-500 px-6 font-medium text-white duration-500"
+                    class="group w-full self-end inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-purple-500 px-6 font-medium text-white transition hover:bg-purple-500/70 hover:text-white/70"
                 >
-                <div class="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
                     Adicionar
-                </div>
-                <div class="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100">
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6">
-                        <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
-                        </path>
-                    </svg>
-                </div>
                 </button>
             </form>
         </div>
