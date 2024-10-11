65453444789e599b1375b0aee3fdcf8c7ac60d4c
Jefferson <Dibloob@outlook.com>
2024-10-11 02:42:52 -0300

    👍👍👍

--- listarRefris.php
+++ listarRefris.php
@@ -20,10 +20,26 @@
 </head>
 <body>
     <section class="flex items-center justify-center min-h-screen">
-        <div class="flex flex-col gap-5 p-5 border-2 border-gray-300 rounded-lg">
-            <h3 class="text-2xl font-bold text-white">
-                Estoque de Refrigerantes
-            </h3>
+        <div class="flex flex-col gap-5">
+            <div class="flex items-center justify-between">
+                <h3 class="text-2xl font-bold text-white">
+                    Estoque de Refrigerantes
+                </h3>
+                <a 
+                    href="form_cadastro.php"
+                    class="group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-purple-500 p-3 font-medium text-white duration-500"
+                >
+                    <div class="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
+                        Novo Refrigerante
+                    </div>
+                    <div class="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100">
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6">
+                            <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
+                            </path>
+                        </svg>
+                    </div>
+                </a>
+            </div>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -80,20 +96,7 @@
     
                 </table>
             </div>
-            <a 
-                href="form_cadastro.php"
-                class="group w-max relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-purple-500 px-6 font-medium text-white duration-500"
-            >
-                <div class="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
-                    Adicionar Refrigerante
-                </div>
-                <div class="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100">
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6">
-                        <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
-                        </path>
-                    </svg>
-                </div>
-            </a>
+            
         </div>
     </section>
 </body>
